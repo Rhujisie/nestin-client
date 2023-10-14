97cb156ced9c672440eb9aa5833276de34fdb827
Rhujisie Kajiri <rhujisie.kajiri@gmail.com>
2023-10-14 15:23:26 +0530

    correcting base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://nestin.onrender.com/api/v1';
+const BASE_URL = 'https://nestin-api.onrender.com/api/v1';
 
 export default axios.create({
     baseURL: BASE_URL,
