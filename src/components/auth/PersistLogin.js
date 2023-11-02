b42f49d36f368ba02fc39eb78dcf07d79bc3a535
Rhujisie Kajiri <rhujisie.kajiri@gmail.com>
2023-11-02 23:09:16 +0530

    rectifying persit login

--- src/components/auth/PersistLogin.js
+++ src/components/auth/PersistLogin.js
@@ -1,26 +1,40 @@
 import { Outlet } from "react-router-dom";
-import {useEffect } from "react";
+import { useState, useEffect } from "react";
 import useRefresh from "../../hooks/useRefresh";
 import useAuth from "../../hooks/useAuth";
 
 
 export default function PersistLogin(){
+    const [isLoading, setIsLoading] = useState(true)
     const refresh = useRefresh()
     const {auth} = useAuth()
-
+    console.log('persist login')
     useEffect(()=>{
+        let isMounted = true
         const verifyRefreshToken = async()=>{
             try{
+                console.log('verify')
                 await refresh()
             }catch(err){
                 console.log(err)
             }
+            //this will run irregardless(may cause memory leak)
+            finally{
+                isMounted && setIsLoading(false)
+            }
         }
-        (!auth?.accessToken) && verifyRefreshToken()
+        (!auth?.accessToken)? verifyRefreshToken(): setIsLoading(false)
+
+        //cleaning memory leak(state is changed when component is not mounted)
+        return ()=>{
+            isMounted = false
+        }
     },[])
     return(
         <>
-            <Outlet/>
+            {isLoading? 
+                <p>Loading...</p>:
+                <Outlet/>}
         </>
     )
 }
