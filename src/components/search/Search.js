43af2b0a30fd9ed7c324bfa8c06cc459ff0dda1a
Rhujisie Kajiri <rhujisie.kajiri@gmail.com>
2023-11-03 14:37:44 +0530

    small css search.css changes

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
 import '../search/Search.css'
 import PinLogo from '../../icon/pin.gif'
 import useLocation from '../../hooks/useLocation'
@@ -11,11 +12,6 @@ export default function Search({search ,setSearch}) {
     const [searchResult, setSearchResult] = useState(false)
     const {setLocation} = useLocation()
 
-    const resultElem = city.map((data)=>{
-        if(data.toLocaleLowerCase().startsWith(search.toLocaleLowerCase())){
-            return <div key={data} onClick={(e)=>handleClick(e, data)}>{data}</div>
-        }
-    })
     const handleClick=(e, data)=>{
         setIsHidden(true)
         setSearchResult(true)
@@ -27,6 +23,17 @@ export default function Search({search ,setSearch}) {
         setSearchResult(false)
         setSearch(e.target.value)
     }
+    const divVariant = {
+        initial:{y: -20, opacity: 0},
+        animate:{y: 0, opacity: 1}, 
+        transition:{type:'spring',duration: 2, delayChildren: .3, staggerChildren: .3}, 
+        exit:{y: -20,opacity: 0}
+    }
+    const resultElem = city.map((data)=>{
+        if(data.toLocaleLowerCase().startsWith(search.toLocaleLowerCase())){
+            return <div key={data} onClick={(e)=>handleClick(e, data)}>{data}</div>
+        }
+    })
   return (
     <div className='search'>
         <input placeholder='Search...'
@@ -35,10 +42,14 @@ export default function Search({search ,setSearch}) {
             <img src={PinLogo} alt='pin' className='pin-logo'/>
         </label>
         {search && <p className={`para search-para ${isHidden? 'hidden': ''}`}>Select location from the option</p>}
-        {search && <div className={`search-result-container 
-            ${searchResult? 'hidden': ''}`}>
-            {resultElem}
-        </div>}
+        <AnimatePresence>
+            {search && <motion.div className={`search-result-container 
+                ${searchResult? 'hidden': ''}`} variants={divVariant}
+                initial='initial' animate='animate' transition='transition'
+                exit='exit'>
+                {resultElem}
+            </motion.div>}
+        </AnimatePresence>
     </div>
   )
 }
